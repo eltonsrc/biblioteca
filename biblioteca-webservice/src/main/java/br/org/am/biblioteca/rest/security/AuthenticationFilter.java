c7b754784ac0fa216f5330841bb5e74706369979
Elton Serra Ribeiro Couto <eltonsrc@gmail.com>
2016-02-17 15:09:40 -0400

    Não mandar sendChalenge no filter do shiro.

--- biblioteca-webservice/src/main/java/br/org/am/biblioteca/rest/security/AuthenticationFilter.java
+++ biblioteca-webservice/src/main/java/br/org/am/biblioteca/rest/security/AuthenticationFilter.java
@@ -1,9 +1,25 @@
 package br.org.am.biblioteca.rest.security;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.shiro.web.filter.authc.BasicHttpAuthenticationFilter;
+import org.apache.shiro.web.util.WebUtils;
 import org.springframework.stereotype.Component;
 
+/**
+ * Classe criada para sobrescrever comportamento do shiro de fazer
+ * request/challenge do login e senha(Browsers abrem uma popup pedindo login e
+ * senha).
+ * 
+ */
 @Component
 public class AuthenticationFilter extends BasicHttpAuthenticationFilter {
-
+    @Override
+    protected boolean sendChallenge(ServletRequest request, ServletResponse response) {
+        HttpServletResponse httpResponse = WebUtils.toHttp(response);
+        httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        return false;
+    }
 }
