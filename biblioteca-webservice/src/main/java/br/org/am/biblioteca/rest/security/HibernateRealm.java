b034626b4ad7c7eb63d4e5fa24e6e0408397bb00
Elton Serra Ribeiro Couto <eltonsrc@gmail.com>
2016-02-17 11:28:00 -0400

    Uso de transação do hibernate no realm para o shiro.

--- biblioteca-webservice/src/main/java/br/org/am/biblioteca/rest/security/HibernateRealm.java
+++ biblioteca-webservice/src/main/java/br/org/am/biblioteca/rest/security/HibernateRealm.java
@@ -11,34 +11,61 @@ import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import br.org.am.biblioteca.dao.UsuarioDAO;
 import br.org.am.biblioteca.model.Grupo;
 import br.org.am.biblioteca.model.Usuario;
 
 /**
- * Realm para acessar banco de dados através do hibernate.
+ * Realm para acessar banco de dados através do hibernate. Como o spring não
+ * consegue criar transações automaticamente nesta classe, nós devemos criar
+ * manualmente.
  *
  */
 @Component
 public class HibernateRealm extends AuthorizingRealm {
+    private final TransactionTemplate transactionTemplate;
     private UsuarioDAO usuarioDAO;
 
-    public HibernateRealm() {
-        setName("hibernateRealm");
-    }
-
     @Autowired
-    public void setUsuarioDAO(UsuarioDAO usuarioDAO) {
+    public HibernateRealm(PlatformTransactionManager transactionManager,
+            UsuarioDAO usuarioDAO) {
         this.usuarioDAO = usuarioDAO;
+        transactionTemplate = new TransactionTemplate(transactionManager);
+        transactionTemplate.setReadOnly(true);
+        setName("hibernateRealm");
     }
 
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(
-            PrincipalCollection principalCollection) {
+            final PrincipalCollection principalCollection) {
+        return transactionTemplate.execute(new TransactionCallback<AuthorizationInfo>() {
+            public AuthorizationInfo doInTransaction(TransactionStatus status) {
+                return doGetAuthorizationInfo(principalCollection, status);
+            }
+        });
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(
+            final AuthenticationToken authenticationToken)
+                    throws AuthenticationException {
+        return transactionTemplate.execute(new TransactionCallback<AuthenticationInfo>() {
+            public AuthenticationInfo doInTransaction(TransactionStatus status) {
+                return doGetAuthenticationInfo(authenticationToken, status);
+            }
+        });
+    }
+
+    private AuthorizationInfo doGetAuthorizationInfo(
+            final PrincipalCollection principalCollection, TransactionStatus status) {
         String email = principalCollection.getPrimaryPrincipal().toString();
 
-        Usuario usuario = usuarioDAO.findByEmailWithProgramaticTransaction(email);
+        Usuario usuario = usuarioDAO.findByEmail(email);
 
         if (usuario == null) {
             return null;
@@ -52,13 +79,12 @@ public class HibernateRealm extends AuthorizingRealm {
         return info;
     }
 
-    @Override
-    protected AuthenticationInfo doGetAuthenticationInfo(
-            AuthenticationToken authenticationToken) throws AuthenticationException {
+    private AuthenticationInfo doGetAuthenticationInfo(
+            AuthenticationToken authenticationToken, TransactionStatus status)
+                    throws AuthenticationException {
         UsernamePasswordToken token = (UsernamePasswordToken) authenticationToken;
 
-        Usuario usuario = usuarioDAO
-                .findByEmailWithProgramaticTransaction(token.getUsername());
+        Usuario usuario = usuarioDAO.findByEmail(token.getUsername());
 
         if (usuario == null) {
             return null;
