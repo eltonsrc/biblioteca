07398f2201a1ef9744d8d7f8f6db86efcc8ac42d
Elton Serra Ribeiro Couto <eltonsrc@gmail.com>
2016-02-17 11:51:39 -0400

    Criação de url de login e logout.

---
+++ biblioteca-webservice/src/main/java/br/org/am/biblioteca/rest/AuthController.java
@@ -0,0 +1,32 @@
+package br.org.am.biblioteca.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Path("/auth")
+public class AuthController {
+    @GET
+    @Path("/login")
+    public Response login() {
+        Subject subject = SecurityUtils.getSubject();
+        if (subject.isAuthenticated()) {
+            return Response.status(Status.OK).entity("Login ok").build();
+        }
+
+        return Response.status(Status.UNAUTHORIZED).build();
+    }
+
+    @GET
+    @Path("/logout")
+    public Response logout() {
+        SecurityUtils.getSubject().logout();
+        return Response.ok().entity("Logout ok").build();
+    }
+}
--- biblioteca-webservice/src/main/java/br/org/am/biblioteca/rest/InfoController.java
+++ biblioteca-webservice/src/main/java/br/org/am/biblioteca/rest/InfoController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 @Path("/")
-public class InfoService {
+public class InfoController {
     @GET
     @Path("/info")
     public Response info() {
